38c38fb813152867ed4b6533f6624ccfc8ab36d9
giscruzm <cruzmarg@tcd.ie>
2024-11-11 12:21:05 +0000

    Add files via upload

---
+++ Lab.js
@@ -0,0 +1,17 @@
+let firstLocation= "Cabra";
+console.log(firstLocation);
+
+let secondLocation ="The Liberties";
+console.log(secondLocation);
+
+let thirdLocation="The Point";
+console.log(thirdLocation);
+
+let fourthLocation="Finglas";
+console.log(fourthLocation);
+
+let fifthLocation="Ranelagh";
+console.log(fifthLocation);
+
+let food= ["chilaquiles","pozole","barbacoa","cabrito"];
+console.log(food[0]);
